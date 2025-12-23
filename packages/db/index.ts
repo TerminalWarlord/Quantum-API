fd071faa54378a1d965e7efcf6106d3b48137de5
terminalwarlord <joybiswas389@gmail.com>
2025-12-24 00:25:24 +0600

    feat(db): add new schema tables and update existing schemas

--- packages/db/index.ts
+++ packages/db/index.ts
@@ -1,10 +1,16 @@
 import 'dotenv/config';
 
-export * from "./src/db/schema/users";
 export * from "./src/db/schema/api_keys";
 export * from "./src/db/schema/apis";
+export * from "./src/db/schema/categories";
+export * from "./src/db/schema/endpoints";
+export * from "./src/db/schema/enums";
+export * from "./src/db/schema/invoices";
+export * from "./src/db/schema/parameters";
 export * from "./src/db/schema/plans";
 export * from "./src/db/schema/subscriptions";
+export * from "./src/db/schema/transactions";
 export * from "./src/db/schema/usage";
+export * from "./src/db/schema/users";
 export * from "./client";
 
