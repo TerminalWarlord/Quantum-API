e859c836a8d8a896f063d720f46443cb1fa42757
terminalwarlord <joybiswas389@gmail.com>
2025-12-25 19:22:33 +0600

    feat: add endpoint for category

--- apps/backend/src/routes/content.ts
+++ apps/backend/src/routes/content.ts
@@ -1,9 +1,8 @@
 import { Hono } from "hono";
-import { bearerAuth } from "hono/bearer-auth";
-import { registerController } from "../controllers/authControllers/register.controller";
-import { postCreateApi } from "../controllers/apiControllers/createApi.controller";
-import { postCreatePlan } from "../controllers/apiControllers/createPlan.controller";
+import { postCreateApi } from "../controllers/api_controllers/create_api.controller";
+import { postCreatePlan } from "../controllers/api_controllers/create_plan.controller";
 import { middleware } from "../middlewares/middleware";
+import { postCreateCategory } from "../controllers/api_controllers/create_category.controller";
 
 
 
@@ -11,7 +10,7 @@ const contentRouter = new Hono();
 
 contentRouter.post('/create-api', middleware, postCreateApi);
 contentRouter.post('/create-api-plan', middleware, postCreatePlan);
-// contentRouter.post('/create-api-plan', middleware, postCreatePlan);
+contentRouter.post('/create-category', middleware, postCreateCategory);
 
 
 export default contentRouter;
