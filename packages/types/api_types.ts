855c96dd067ea9a89ac0b45039a9a53e343bbd8b
terminalwarlord <joybiswas389@gmail.com>
2025-12-28 19:27:09 +0600

    feat(types): add types for endpoint and parameter

---
+++ packages/types/api_types.ts
@@ -0,0 +1,28 @@
+export enum OrderByEnum {
+    PRICE = "PRICE",
+    RATING = "RATING",
+    TRENDING = "TRENDING",
+    PUBLISHED = "PUBLISHED"
+}
+
+
+export enum EndpointMethod {
+    GET = "GET",
+    POST = "POST",
+    PUT = "PUT",
+    PATCH = "PATCH",
+    DELETE = "DELETE"
+}
+
+export enum ParameterLocation {
+    QUERY = "QUERY",
+    BODY = "BODY",
+    HEADER = "HEADER"
+}
+
+export enum ParameterType {
+    STRING = "STRING",
+    NUMBER = "NUMBER",
+    BOOLEAN = "BOOLEAN",
+    OBJECT = "OBJECT"
+}
--- packages/types/index.ts
+++ packages/types/index.ts
@@ -1,3 +1,4 @@
 export * from "./payments/transaction_types";
 export * from "./payments/subscription_types";
 export * from "./errors";
+export * from "./api_types";
