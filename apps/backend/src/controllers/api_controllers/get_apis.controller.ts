545f8ed703829445fe23218e3924e11cebb74aed
terminalwarlord <joybiswas389@gmail.com>
2025-12-28 16:54:41 +0600

    feat(api): add pagination and validation; fix status codes

--- apps/backend/src/controllers/api_controllers/get_apis.controller.ts
+++ apps/backend/src/controllers/api_controllers/get_apis.controller.ts
@@ -1,15 +1,47 @@
-import { apiTable, db, planTable } from "@repo/db/client";
+import { db } from "@repo/db/client";
 import { Context } from "hono";
-import { eq } from "drizzle-orm";
-
+import { sql } from "drizzle-orm";
+import * as z from "zod";
+import { OrderByEnum } from "@repo/types";
 
 export const getApis = async (c: Context) => {
-    const apis = await db
-        .select()
-        .from(apiTable)
-        .fullJoin(planTable, eq(apiTable.id, planTable.api_id))
-        .limit(30)
+    const schema = z.object({
+        term: z.string().optional(),
+        limit: z.coerce.number().min(1).max(50).default(10),
+        offset: z.coerce.number().min(0).default(0),
+        category_slug: z.string().optional(),
+        order_by_field: z.enum(OrderByEnum).default(OrderByEnum.PUBLISHED),
+        order_by: z.enum(["asc", "des"]).default("asc")
+    })
+    const parseData = schema.safeParse(c.req.query());
+    if (!parseData.success) {
+        return c.json({
+            message: "Failed to query",
+            error: parseData.error.flatten().fieldErrors,
+        }, 400);
+    }
+    const { limit, offset, order_by, category_slug, order_by_field, term } = parseData.data;
+    // TODO: Implement Ordering
+    const apis = await db.execute(sql`
+        SELECT a.title, a.slug, a.description, c.name AS category_name, c.slug as category_slug, a.thumbnail_url, a.updated_at  FROM apis AS a
+        JOIN categories AS c
+        ON c.id=a.category_id
+        WHERE (
+            ${term ? sql`a.title ILIKE ${'%' + term + '%'}` : sql`TRUE`}
+            AND 
+            ${category_slug ? sql`c.slug=${category_slug}` : sql`TRUE`}
+            )
+        LIMIT ${limit + 1}
+        OFFSET ${offset}
+    `)
+    if (!apis || !apis.rowCount) {
+        return c.json({
+            message: "Nothing found"
+        }, 404);
+    }
+    const hasNextPage = apis.rowCount > limit;
     return c.json({
-        ...apis
+        results: apis.rows,
+        hasNextPage,
     })
 }
